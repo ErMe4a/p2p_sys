fe23a2227499334127d18cafb7e981007ba3f5eb
ErMe4a <babaeve63@gmail.com>
2026-03-25 22:58:47 +0300

    выравнивание 2

--- sync_orders_bybit.py
+++ sync_orders_bybit.py
@@ -21,7 +21,7 @@ import django
 from decimal import Decimal, ROUND_DOWN
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")  # <-- поменяй если надо
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")  # <-- поменяй если надо
 
 try:
     django.setup()
